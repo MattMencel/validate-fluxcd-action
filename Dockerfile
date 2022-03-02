4cb10c699f7d87c935d9cf8a63620542a4145d26
Matt Mencel <matt@techminer.net>
2022-03-01 22:39:49 -0600

    Curl (#2)
    * update shebang
    
    * install curl

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.15
 
+RUN apk add --update --no-cache curl
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-
 # This script downloads the Flux OpenAPI schemas, then it validates the
 # Flux custom resources and the kustomize overlays using kubeconform.
 
